d8011039e3c791fce923ddaf43cd1aadf6fe2c55
Guillaume B <g.broder@groupe-curious.com>
2020-01-30 11:27:57 +0100

    Upload dist

---
+++ dist/pipeline/index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "buildEvent", {
+  enumerable: true,
+  get: function get() {
+    return _build["default"];
+  }
+});
+Object.defineProperty(exports, "formatEvent", {
+  enumerable: true,
+  get: function get() {
+    return _format["default"];
+  }
+});
+
+var _build = _interopRequireDefault(require("./build"));
+
+var _format = _interopRequireDefault(require("./format"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
